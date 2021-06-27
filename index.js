0bc0b7ea6307f8f187aa84c27bb6032780e06c4f
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-27 06:25:50 +0600

    database connected

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 // db connection
-const { MongoClient } = require("mongodb");
+console.log(process.env.DB_Name);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@burjalarab.jzxpk.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 client.connect((err) => {
-  const products = client
+  const productsCollection = client
     .db(`${process.env.DB_NAME}`)
     .collection(`${process.env.COLLECTION_NAME}`);
   // perform actions on the collection object
-  console.log("db connected");
+  console.log("db connected error is: ", err);
   app.post("/addProduct", (req, res) => {
-    const product = req.body.product;
-    products.insertOne(product).then((result) => console.log(result));
+    const product = req.body;
+    productsCollection
+      .insertOne(product)
+      .then((result, err) => console.log(result));
   });
 });
 
